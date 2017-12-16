4d4ca796c2679568515f327793a8c0c8154ea476
thezodiac1994 <gautamtrueblue@gmail.com>
2017-12-15 22:25:41 -0500

    code now only needs testing data

--- project/BOW/code/guessImage.m
+++ project/BOW/code/guessImage.m
@@ -6,7 +6,7 @@ function guessedImage = guessImage( imagename )
 	%fprintf('[Loading..]\n');
     load('vision.mat');
 	load('../data/traintest.mat','mapping');
-
+    mapping = mapping {1,1};
 	image = im2double(imread(imagename));
 
 %	imshow(image);
@@ -15,7 +15,7 @@ function guessedImage = guessImage( imagename )
 	h = getImageFeaturesSPM(3, wordMap, size(dictionary,2));
 	distances = distanceToSet(h, train_features);
     
-    knn = 1;
+    knn = 3;
     knn_labels = cell(1,knn);
     train_labels = train_labels{1,1}
     for i = 1 : knn
@@ -31,9 +31,14 @@ function guessedImage = guessImage( imagename )
     % mean_closest = mode(knn_labels{:});
 	% guessedImage = mapping{mean_closest};
 	
-    guessedImage = knn_labels{1,1};
     
-    fprintf('[My Guess]:%s.\n',guessedImage);
+    for i = 1:knn
+        knn_labels{1,i} = str2num(['uint8(',knn_labels{1,i},')'])
+    end
+    labelmat = cell2mat(knn_labels);
+    guessedImage = mode(labelmat(:));
+    
+    fprintf('[My Guess]:%s.\n',mapping{1,guessedImage});
 
 	%figure(1);
 	%imshow(image);
--- project/BOW/code/main.m
+++ project/BOW/code/main.m
@@ -1,3 +1,4 @@
+%createDataFiles();
 %computeDictionary();
 %batchToVisualWords();
 %buildRecognitionSystem();
